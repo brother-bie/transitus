6f94306c0c46bc3454445e8102fc6ecc210430fb
Jason Bert <jason.bert@goblinrockets.com>
2014-10-23 08:55:53 +0100

    Re-factored business logic for populating template object to the template factory

--- Source/Transitus/TemplateFactory.cs
+++ Source/Transitus/TemplateFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sitecore.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,78 @@ namespace Transitus
 	{
 		public IEnumerable<ITemplate> Create(IEnumerable<IItem> items)
 		{
-			var templates = items
-								.Where(i => new Sitecore.Data.ID(i.TemplateId) == Sitecore.TemplateIDs.Template)
-								.Select(i => new Template(i, items))
-								.ToList();
+			var templateItems = items.Where(i => IsTemplate(i.TemplateId));
+			var templates = new List<ITemplate>();
+
+			foreach (var templateItem in templateItems)
+			{
+				var combinedTemplateItems = GetBaseTemplates(templateItem, items);
+				var combinedSections = items.Where(item => combinedTemplateItems.Select(i => i.Id).Contains(item.ParentId)).ToList();
+				var combinedFields = GetFields(combinedSections, items);
+				var localSections = items.Where(item => item.ParentId == templateItem.Id).ToList();
+				var localFields = GetFields(localSections, items);
+				var template = new Template
+				{
+					Id = templateItem.Id,
+					Name = templateItem.Name,
+					ParentId = templateItem.ParentId,
+					Path = templateItem.ItemPath
+				};
+
+				template.CombinedTemplateItems = combinedTemplateItems;
+				template.CombinedFields = combinedFields;
+				template.LocalFields = localFields;
+
+				templates.Add(template);
+			}
 
 			return templates;
+		}
+
+		public IEnumerable<IItem> GetBaseTemplates(IItem item, IEnumerable<IItem> items)
+		{
+			var baseTemplates = new List<IItem>();
+
+			GetBaseTemplates(item, baseTemplates, items);
+
+			return baseTemplates;
+		}
+
+		public void GetBaseTemplates(IItem item, IList<IItem> inheritedTemplates, IEnumerable<IItem> items)
+		{
+			if (item != null && !inheritedTemplates.Where(i => i.Id == item.Id).Any())
+			{
+				inheritedTemplates.Add(item);
+
+				var baseField = item.SharedFields.Where(i => IsBaseTemplate(i.Id)).FirstOrDefault();
+
+				if (baseField != null)
+				{
+					foreach (var value in baseField.Value.Split('|'))
+					{
+						var baseItem = items.Where(i => i.Id == value).FirstOrDefault();
+
+						GetBaseTemplates(baseItem, inheritedTemplates, items);
+					}
+				}
+			}
+		}
+
+		public IEnumerable<ITemplateField> GetFields(IEnumerable<IItem> sections, IEnumerable<IItem> items)
+		{
+			var fields = items.Where(field => sections.Select(i => i.Id).Contains(field.ParentId)).ToList();
+
+			return fields.Select(field => new TemplateField(field)).OrderBy(i => i.Name).ToList();
+		}
+
+		public bool IsBaseTemplate(string id)
+		{
+			return new ID(id) == Sitecore.FieldIDs.BaseTemplate;
+		}
+
+		public bool IsTemplate(string id)
+		{
+			return new ID(id) == Sitecore.TemplateIDs.Template;
 		}
 	}
 }
